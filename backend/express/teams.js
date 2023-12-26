590033b26e39183057b863f7780388f5ca7166ba
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2023-12-26 18:58:54 +0530

    ejs

--- backend/express/teams.js
+++ backend/express/teams.js
@@ -56,16 +56,55 @@ app.post('/addemp_submit', function(req, res) {
     });
 });
 
+app.get('/editemp', function(req, res) {
+    let empid = req.query.empid;
+    db.query("SELECT * FROM emp WHERE empid=" + empid, function(err, empdata) {
+        db.query("SELECT * FROM departments", function(err, deptdata) {
+            res.render('editemp', { empdata: empdata[0], deptdata: deptdata });
+        });
+    });
+});
+
+app.post('/editemp_submit', function(req, res) {
+    let { empid, ename, add1, salary, deptid} = req.body;
+    let values = [ename, add1, salary, deptid, empid];
+
+    let sql = `UPDATE emp SET ename=?, add1=?, salary=?, deptid=? WHERE empid=?`;
+    
+    db.query(sql, values, function(err, result) {
+        console.log(err)
+            res.redirect('/listemp');
+    });
+});
 
 
-// app.get('/editemp',function(req,res){
-//     let empid = req.query();
-//     db.query("select * from emp where empid="+empid,function(err,empdata){
-//         db.query("select * from emp data",function(err,deptdata){
-//             res.render('editemp',{editemp:editemp,deptdata:deptdata})
-//         })
-//     })
-// })
+
+
+app.get('/deleteemp', function(req, res) {
+    let empid = req.query.empid;
+    db.query("DELETE FROM emp WHERE empid=" + empid, function(err, result) {
+            res.redirect('/listemp');
+        
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -102,6 +141,40 @@ app.post('/adddep_submit', function(req, res) {
         }
     });
 });
+
+app.get('/editdept', function(req, res) {
+    let deptid = req.query.deptid;
+    db.query("SELECT * FROM departments WHERE deptid=" + deptid, function(err, deptdata) {
+        res.render('editdept', { deptdata: deptdata[0] });
+    });
+});
+
+app.post('/editdept_submit', function(req, res) {
+    let { deptid, dname } = req.body;
+    let sql = `UPDATE departments SET dname='${dname}' WHERE deptid=${deptid}`;
+
+    db.query(sql, function(err, result) {
+            res.redirect('/listdept');
+        
+    });
+});
+
+app.get('/deletedept', function(req, res) {
+    let deptid = req.query.deptid;
+    console.log(deptid);
+    db.query("DELETE FROM departments WHERE deptid=" + deptid, function(err, result) {
+        res.redirect('/listdept');
+    });
+});
+
+
+
+
+
+
+
+
+
 
 
 
@@ -148,6 +221,38 @@ app.get('/listprojects', function(req, res) {
         res.render('listprojects', { result: result });
     });
 });
+
+app.get('/editproject', function(req, res) {
+    let project_id = req.query.project_id;
+    db.query("SELECT * FROM projects WHERE project_id=" + project_id, function(err, projectData) {
+        res.render('editproject', { projectData: projectData[0] });
+    });
+});
+
+app.post('/editproject_submit', function(req, res) {
+    let { project_id, project_name,project_description,languages_used } = req.body;
+    let sql = `UPDATE projects SET project_name='${project_name}' ,project_description='${project_description}',languages_used='${languages_used}' WHERE project_id=${project_id}`;
+
+    db.query(sql, function(err, result) {
+        res.redirect('/listprojects');
+    });
+});
+
+app.get('/deleteproject', function(req, res) {
+    let project_id = req.query.project_id;
+    db.query("DELETE FROM projects WHERE project_id=" + project_id, function(err, result) {
+        res.redirect('/listprojects');
+    });
+});
+
+
+
+
+
+
+
+
+
 
 
 
@@ -197,6 +302,49 @@ app.post('/addservice_submit', function(req, res) {
     });
 });
 
+app.get('/editservice', function(req, res) {
+    let service_id = req.query.service_id;
+    db.query("SELECT * FROM services WHERE service_id=" + service_id, function(err, servicedata) {
+        res.render('editservice', { servicedata: servicedata[0] });
+    });
+});
+
+app.post('/editservice_submit', function(req, res) {
+    let { service_id, service_name} = req.body;
+    let sql = `UPDATE services SET service_name='${service_name}' WHERE service_id=${service_id}`;
+
+    db.query(sql, function(err, result) {
+            res.redirect('/listservices');
+        
+    });
+});
+
+app.get('/deleteservice', function(req, res) {
+    let service_id = req.query.service_id;
+    db.query("DELETE FROM services WHERE service_id=" + service_id, function(err, result) {
+        res.redirect('/listservices');
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -238,8 +386,8 @@ app.get('/addfeedbacks',function(req,res){
 
 
 app.post('/addfeedback_submit', function(req, res) {
-    let { client_name,creativity,designing,team_work,feedback } = req.body;
-    let sql = "INSERT INTO feedback ( client_name,creativity,designing,team_work,feedback) VALUES ('" + client_name + "','" + creativity + "','" + designing + "','" + team_work + "','" + feedback + "')";
+    let { client_name,creativity,designing,team_work,feedback,service_id } = req.body;
+    let sql = "INSERT INTO feedback ( client_name,creativity,designing,team_work,feedback,service_id) VALUES ('" + client_name + "','" + creativity + "','" + designing + "','" + team_work + "','" + feedback + "','" + service_id + "')";
 
     db.query(sql, function(err, result) {
         if (result.insertId > 0) {
@@ -247,4 +395,37 @@ app.post('/addfeedback_submit', function(req, res) {
         }
     });
 });
+
+app.get('/editfeedback', function(req, res) {
+    let client_id = req.query.client_id;
+    db.query("SELECT * FROM feedback WHERE client_id=" + client_id, function(err, feedbackdata) {
+        db.query("SELECT * FROM services", function(err, serviceS) {
+            res.render('editfeedback', { feedbackdata: feedbackdata[0], serviceS: serviceS });
+        });
+    });
+});
+
+app.post('/editfeedback_submit', function(req, res) {
+    let { client_id, client_name, creativity, designing, team_work, feedback, service_id } = req.body;
+    let values = [client_name, creativity, designing, team_work, feedback, service_id, client_id];
+    
+    let sql = `UPDATE feedback SET client_name=?, creativity=?, designing=?, team_work=?, feedback=?, service_id=? WHERE client_id=?`;
+    
+    db.query(sql, values, function(err, result) {
+        console.log(err);
+        res.redirect('/listfeedbacks');
+    });
+    
+});
+
+
+
+
+app.get('/deletefeedback', function(req, res) {
+    let client_id = req.query.client_id;
+    db.query("DELETE FROM feedback WHERE client_id=" + client_id, function(err, result) {
+        res.redirect('/listfeedbacks');
+    });
+});
+
 app.listen(8000,function(){console.log("at 8000")});
